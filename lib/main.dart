41f9879832f748dcde5eaf7789b1b82e9a10aee6
gulssumm <gulsumcetinozlu@gmail.com>
2024-09-21 15:01:33 +0300

    figma

--- lib/main.dart
+++ lib/main.dart
@@ -11,12 +11,46 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    double deviceHeight = MediaQuery.of(context).size.height;
+    double deviceWidth = MediaQuery.of(context).size.width;
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        body: Container(
-          width: ,
-          height: ,
+      home: SafeArea(
+        child: Scaffold(
+          body: Container(
+            width: deviceWidth,
+            height: deviceHeight / 3,
+            // color: Colors.purple,
+            decoration: const BoxDecoration(
+                color: Colors.purple,
+                image: DecorationImage(
+                    image: AssetImage("lib/assets/images/header.png"),
+                    fit: BoxFit.cover)),
+            child: const Column(
+              children: [
+                Padding(
+                  padding: EdgeInsets.only(top: 20),
+                  child: Text(
+                    "October 20, 2022",
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold),
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(top: 40),
+                  child: Text(
+                    "My Todo List",
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 35,
+                        fontWeight: FontWeight.bold),
+                  ),
+                )
+              ],
+            ),
+          ),
         ),
       ),
     );
